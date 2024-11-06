a25fab86e5dc03cb1041071b4e95149a28a89a17
Tharun-nav <tharun.naveen@ontariotechu.net>
2024-11-05 19:14:34 -0500

    6:45PM 2024 - 11 - 05

--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', {
+    title: 'Home',
+  });
+});
+
+router.get('/Home', function(req, res, next) {
+  res.render('index', {
+    title: 'Home',
+  });
+});
+
+router.get('/AboutMe', function(req, res, next) {
+  res.render('index', {
+    title: 'About me',
+  });
+});
+
+router.get('/Projects', function(req, res, next) {
+  res.render('index', {
+    title: 'My Projects',
+  });
+});
+/* Get Contact Me page */
+router.get('/ContactMe', function(req, res, next) {
+  res.render('index', {
+    title: 'Contact me',
+  });
 });
 
 module.exports = router;
